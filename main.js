9ac26015c3158e13d28f38b807cae41af6c5b0bf
Oliseyenum Favour <105770742+Favourose1@users.noreply.github.com>
2022-06-24 03:37:22 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var opr = prompt("operator: +, -, *, / ")
+var num1 = parseInt(prompt("Nunber 1: "))
+var num2 = parseInt(prompt("Nunber 2: "))
+
+if(opr == "+")
+{
+    console.log(num1 + num2)
+} else if(opr == "-")
+{
+    console.log(num1 - num2)
+} else if(opr == "*")
+{
+    console.log(num1 * num2)
+} else if(opr == "/")
+{
+    console.log(num1 / num2)
+}
